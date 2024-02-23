2e227be7e9e9312e56ca1d6288cb2c93b8db838e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-23 13:19:00 +0100

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import BackgroundHero from "./../components/svg/BackgroundHero";
+import Nav from "./Nav";
+
+const Hero = () => {
+  return (
+    <div className="w-full relative">
+      <BackgroundHero />
+      <Nav />
+    </div>
+  );
+};
+
+export default Hero;
