ba84ac9608a6377d0448d17a131b7428adc0d231
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-27 15:01:22 +0100

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,26 @@
+"use client";
+
+import gsap from "gsap";
+import { useEffect } from "react";
 import { Button } from "./ui/button";
 
 const Nav = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      "header",
+      {
+        y: -100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        delay: 1.2,
+        ease: "power4.out",
+      }
+    );
+  }, []);
   return (
     <header className="max-w-5xl mx-auto py-4 px-2 flex justify-between">
       <a href="/">
