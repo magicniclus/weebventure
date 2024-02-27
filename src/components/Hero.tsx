76343d7a71d77d1c8e8c8dc64f4dbdb80623013b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-27 10:54:32 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,8 +15,30 @@ const Hero = () => {
     { name: "LANDING PAGE", path: "/background/dev.jpg" },
     { name: "SIMULATEURS", path: "/background/simulateur.jpg" },
   ];
+  const [hoverStates, setHoverStates] = useState(
+    imageOptions.reduce((acc, item) => ({ ...acc, [item.name]: false }), {})
+  );
 
   const [activeImage, setActiveImage] = useState(imageOptions[0].path);
+  const [prevImage, setPrevImage] = useState("");
+
+  useEffect(() => {
+    if (prevImage) {
+      // Faire disparaître l'ancienne image et la déplacer vers le haut
+      gsap.fromTo(
+        ".prevImage",
+        { y: 0, opacity: 1 },
+        { opacity: 0, duration: 0.5 }
+      );
+    }
+
+    // Attendre que l'ancienne image commence à disparaître avant de commencer l'animation de la nouvelle
+    gsap.fromTo(
+      ".activeImage",
+      { opacity: 0 }, // Commence avec une position plus basse et invisible
+      { y: 0, opacity: 1, duration: 0.5 } // Termine à sa position normale et devient visible
+    );
+  }, [activeImage, prevImage]);
 
   useEffect(() => {
     if (imageRef.current) {
@@ -27,23 +49,43 @@ const Hero = () => {
     }
   }, [activeImage]);
 
-  const handleMouseEnter = (imagePath: string) => {
-    setActiveImage(imagePath);
-  };
-
   useEffect(() => {
     gsap.set(ecommerceImageRef.current, { opacity: 0 }); //
   }, []);
 
-  const handleMouseLeave = () => {
-    gsap.to(devImageRef.current, { opacity: 1, duration: 1 });
-    gsap.to(ecommerceImageRef.current, { opacity: 0, duration: 1 });
+  const handleMouseEnter = (path: string, name: string) => {
+    if (path !== activeImage) {
+      setPrevImage(activeImage); // Définir l'image actuelle comme précédente
+      setActiveImage(path); // Mettre à jour l'image active
+      setHoverStates((prevState) => ({ ...prevState, [name]: true }));
+    }
+  };
+
+  // Fonction pour gérer la sortie de la souris d'une div spécifique
+  const handleMouseLeave = (itemName: string) => {
+    setHoverStates({ ...hoverStates, [itemName]: false });
   };
 
   return (
     <>
       {/* Styles et autres éléments */}
       <style jsx>{`
+        .text-highlight {
+          display: inline-block;
+          position: relative;
+          z-index: 2;
+        }
+
+        .text-highlight::before {
+          content: "";
+          position: absolute;
+          bottom: 12px;
+          left: 0;
+          width: 100%;
+          height: 5px;
+          background-color: #5174b4;
+          z-index: -1;
+        }
         .imageTransition {
           transition: opacity 1s ease-in-out;
           position: absolute;
@@ -79,7 +121,7 @@ const Hero = () => {
           transition: transform 0.3s ease;
         }
         .lignRight:hover .vertical-text {
-          transform: translateX(100%) scale(1.1);
+          transform: translateX(100%);
           color: #f7f8fb;
         }
 
@@ -119,6 +161,35 @@ const Hero = () => {
           background-size: 100% 100%;
           background-repeat: no-repeat;
           animation: fillUpGradient 1s ease forwards;
+        }
+        @keyframes fillDownGradient {
+          0% {
+            background: linear-gradient(to top, #5174b4 0%, white 100%);
+            background-size: 100% 100%;
+          }
+          100% {
+            background: white;
+            background-size: 100% 0%;
+          }
+        }
+
+        /* Classe pour l'animation inverse */
+        .lignRight.reverseAnimation::before {
+          animation: fillDownGradient 1s ease forwards;
+        }
+        @keyframes fillDownGradient {
+          0% {
+            background: linear-gradient(to top, #5174b4 0%, white 100%);
+            background-size: 100% 100%;
+          }
+          100% {
+            background: white;
+            background-size: 100% 0%;
+          }
+        }
+
+        .lignRight.reverseAnimation::before {
+          animation: fillDownGradient 1s ease forwards;
         }
 
         @keyframes fillUpGradient {
@@ -171,21 +242,42 @@ const Hero = () => {
 
           {/* Section droite avec images */}
           <div className="md:w-7/12 w-full md:mt-0 mt-14 min-h-[400px] relative">
-            <Image
-              src={activeImage}
-              alt="Dynamic Image"
-              layout="fill"
-              objectFit="cover"
-              className="imageTransition"
-            />
+            <div className="relative h-[400px] overflow-hidden">
+              {/* Image précédente */}
+              {prevImage && (
+                <Image
+                  src={prevImage}
+                  alt="Previous Image"
+                  layout="fill"
+                  objectFit="cover"
+                  className="prevImage"
+                  style={{ position: "absolute", zIndex: 1 }}
+                />
+              )}
+              {/* Image active */}
+              <Image
+                src={activeImage}
+                alt="Active Image"
+                layout="fill"
+                objectFit="cover"
+                className="activeImage"
+                style={{ position: "absolute", zIndex: 2 }}
+              />
+              {/* Votre logique de survol ici */}
+            </div>
             <div className="overlay-gradient"></div>
             {/* Vos éléments de texte verticaux ici */}
             <div className="absolute inset-0 flex justify-between items-end w-full">
               {imageOptions.map((item) => (
                 <div
                   key={item.name}
-                  className="flex-1 lignRight relative w-1/4"
-                  onMouseEnter={() => setActiveImage(item.path)}
+                  className={`flex-1 lignRight relative w-1/4 ${
+                    hoverStates[item.name as keyof typeof hoverStates]
+                      ? ""
+                      : "reverseAnimation"
+                  }`}
+                  onMouseEnter={() => handleMouseEnter(item.path, item.name)} // Assurez-vous d'utiliser item.path ici
+                  onMouseLeave={() => handleMouseLeave(item.name)}
                 >
                   <p className="vertical-text">{item.name}</p>
                 </div>
