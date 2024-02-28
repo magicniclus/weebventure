ce3df408116e86d12d5b56089c54c6a08381013b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-28 11:29:26 +0100

    Update RightTextContainer.tsx

--- src/components/RightTextContainer.tsx
+++ src/components/RightTextContainer.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect } from "react";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
 
@@ -11,6 +13,65 @@ import DessinProcess from "./svg/DessinProcess";
 gsap.registerPlugin(ScrollTrigger);
 
 const RightTextContainer = () => {
+  useEffect(() => {
+    const titleRightText = document.querySelector(".titleRightText");
+    const textRightText = document.querySelector(".textRightText");
+    const buttonRightText = document.querySelector(".buttonRightText");
+    const imageRightText = document.querySelector(".imageRightText");
+    const commentaryRightText = document.querySelector(".commentaryRightText");
+    const nameRightText = document.querySelector(".nameRightText");
+
+    // Crée une timeline avec ScrollTrigger
+    const tl = gsap.timeline({
+      defaults: { duration: 0.7, ease: "easeOut" },
+      scrollTrigger: {
+        trigger: ".titleRightText",
+        start: "top 80%",
+        end: "top 50%",
+      },
+    });
+
+    // Ajoute les animations à la timeline
+    tl.fromTo(titleRightText, { autoAlpha: 0, y: 50 }, { autoAlpha: 1, y: 0 })
+      .fromTo(
+        textRightText,
+        { autoAlpha: 0, y: 50 },
+        { autoAlpha: 1, y: 0 },
+        "<"
+      )
+      .fromTo(
+        buttonRightText,
+        { autoAlpha: 0, y: 50 },
+        { autoAlpha: 1, y: 0 },
+        "<"
+      )
+      .fromTo(
+        imageRightText,
+        { autoAlpha: 0, y: 50 },
+        { autoAlpha: 1, y: 0 },
+        "<"
+      )
+      .fromTo(
+        commentaryRightText,
+        { autoAlpha: 0, y: 50 },
+        { autoAlpha: 1, y: 0 },
+        "<"
+      )
+      .fromTo(nameRightText, { autoAlpha: 0, y: 50 }, { autoAlpha: 1, y: 0 })
+      // Utilise la fonction then pour attendre que la timeline soit complète
+      .then(() => {
+        // Placez ici la logique pour démarrer l'animation de type "yoyo"
+        // Par exemple, pour faire une animation yoyo sur le titre :
+        gsap.to(titleRightText, {
+          y: "-=10",
+          repeat: -1,
+          yoyo: true,
+          ease: "sine.inOut",
+          duration: 1.5,
+        });
+        // Répétez pour d'autres éléments si nécessaire
+      });
+  }, []);
   return (
     <section className="w-full md:mt-16">
       <div className="max-w-5xl mx-auto py-4 px-2 flex justify-between md:flex-row flex-col items-center">
@@ -20,16 +81,16 @@ const RightTextContainer = () => {
           </div>
         </div>
         <div className="md:w-6/12 w-full">
-          <h2 className="text-textDev font-extrabold text-[36px] mt-10 titleLeftText">
+          <h2 className="text-textDev font-extrabold text-[36px] mt-10 titleRightText">
             Un panel de service complet
           </h2>
-          <p className="text-[12px] w-10/12 mt-5 textLeftText">
+          <p className="text-[12px] w-10/12 mt-5 textRightText">
             Nous travaillons aussi bien sur des site vitrine, des simulateur,
             des sites ecommerces. Soit en partant de rien (from scratch) soit en
             utilisant votre CMS préféré, nos équipes s’adapte à vos besoins afin
             de vous proposer un service sur-mesure du design au développement.
           </p>
-          <Button variant={"perso"} className="mt-7 buttonLeftText">
+          <Button variant={"perso"} className="mt-7 buttonRightText">
             On en parle
           </Button>
           <div className="mt-7 p-4 shadow-xl overflow-hidden">
@@ -41,13 +102,13 @@ const RightTextContainer = () => {
                 alt="avenue-immo"
               />
             </div>
-            <p className="text-[10px] text-textDev/50 w-8/12 mt-5 commentaryLeftText">
+            <p className="text-[10px] text-textDev/50 w-8/12 mt-5 commentaryRightText">
               “Nous travaillons avec Webventure depuis 3 ans maintenant et
               l’équipe est totalement en accord avec nos valeurs. Travail
               réalisé dans les temps, team réactive, créative, super experience
               client.
             </p>
-            <p className="text-[12px] text-textDev font-bold w-10/12 mt-5 nameLeftText">
+            <p className="text-[12px] text-textDev font-bold w-10/12 mt-5 nameRightText">
               Pauline MASSE - Chargé de developpement Avenueimmo.com
             </p>
           </div>
