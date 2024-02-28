dbe9456ddda6954afcf614ec767417b3fe22ac66
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-28 17:15:24 +0100

    Update FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -5,21 +5,55 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const FAQ = () => {
+  const title = document.querySelector(".titleFAQ");
+  const text = document.querySelector(".textFAQ");
+  const button = document.querySelector(".buttonFAQ");
+
+  useEffect(() => {
+    const title = document.querySelector(".titleFAQ");
+    const text = document.querySelector(".textFAQ");
+    const button = document.querySelector(".buttonFAQ");
+
+    // S'assurer que tous les éléments sont présents
+    if (title && text && button) {
+      const tl = gsap.timeline({
+        defaults: { duration: 0.7, ease: "easeOut" },
+        scrollTrigger: {
+          trigger: button,
+          start: "top 80%",
+          end: "top 50%",
+          markers: true,
+        },
+      });
+
+      tl.fromTo(title, { opacity: 0, y: 50 }, { opacity: 1, y: 0 })
+        .fromTo(text, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }, "<")
+        .fromTo(button, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }, "<");
+    }
+  }, []);
+
   return (
     <section className="mt-20 w-full">
       <div className="max-w-5xl mx-auto py-4 px-2 flex justify-between">
         <div className="md:w-1/2 w-full">
-          <h2 className="text-textDev font-extrabold text-[36px] mt-10 titleLeftText">
+          <h2 className="text-textDev font-extrabold text-[36px] mt-10 titleLeftText titleFAQ">
             F.A.Q
           </h2>
-          <p className="text-[12px] w-10/12 mt-5 textLeftText">
+          <p className="text-[12px] w-10/12 mt-5 textFAQ">
             Vous avez un projet web? Nous avons les compétences pour vous
             accompagner, developper et optimiser votre idée. Nous devenons de
             véritable partenaire avec nos clients.{" "}
           </p>
         </div>
-        <div className="md:w-1/2 w-full">
+        <div className="md:w-1/2 w-full buttonFAQ">
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
               <AccordionTrigger>
