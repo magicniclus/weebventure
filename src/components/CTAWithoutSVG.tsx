53994ff9af1cb6b331a8d4634bac5055fc2d3368
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-29 11:08:33 +0100

    Update CTAWithoutSVG.tsx

--- src/components/CTAWithoutSVG.tsx
+++ src/components/CTAWithoutSVG.tsx
@@ -57,10 +57,10 @@ const CTAWithoutSVG = () => {
 
   return (
     <section className="w-full md:mt-16 topSection overflow-hidden">
-      <div className="max-w-5xl mx-auto py-4 md:px-4 px-2 md:bg-backgroundDev rounded-md p-3 flex md:items-center container items-start md:flex-row flex-col">
-        <div className="w-full flex justify-between md:items-center items-start md:px-4 md:flex-row flex-col mt-5">
+      <div className="max-w-5xl mx-auto py-10 md:px-4 px-2 md:bg-backgroundDev rounded-md p-3 flex md:items-center container items-start md:flex-row flex-col">
+        <div className="w-full flex justify-between md:items-center items-start md:px-4 md:flex-row flex-col">
           <div className="text-textDev">
-            <h2 className="text-textDev text-[24px] font-extrabold titleCTAWithoutSVG mt-5">
+            <h2 className="text-textDev text-[24px] font-extrabold titleCTAWithoutSVG">
               Vous souhaitez un accompagnement marketing ?
             </h2>
             <p className="md:w-9/12 text-[14px] sustitleCTAWithoutSVG mt-3">
