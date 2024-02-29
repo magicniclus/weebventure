46b201b8b6ec5e1e1382f76fe88f8e63f38fdc5f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-29 11:08:44 +0100

    Update Techno.tsx

--- src/components/Techno.tsx
+++ src/components/Techno.tsx
@@ -1,4 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+
+import Autoplay from "embla-carousel-autoplay";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/littleCard";
 
 import Image from "next/image";
 
@@ -8,6 +17,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 const Techno = () => {
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: false }));
+
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
@@ -35,9 +46,9 @@ const Techno = () => {
         <h2 className="text-textDev font-extrabold  text-[24px] mt-10 titleLeftText mx-auto">
           Nos technologies
         </h2>
-        <div className="max-w-3xl flex-col w-full mx-auto md:flex hidden justify-between items-center logo-container-global md:flex-nowrap">
-          <div className="w-full flex justify-between items-center mt-10">
-            <div className="logo-container-one-techno logo-container-techno">
+        <div className="max-w-3xl flex-col w-full mx-auto flex justify-between items-center logo-container-global md:flex-nowrap logo-container-techno">
+          <div className="w-full flex justify-between items-center mt-10 md:flex hidden">
+            <div className="logo-container-one-techno">
               <Image
                 src="/logos/next.png"
                 width={45.5}
@@ -45,7 +56,7 @@ const Techno = () => {
                 alt="next"
               />
             </div>
-            <div className="logo-container-one-techno logo-container-techno">
+            <div className="logo-container-o">
               <Image
                 src="/logos/react.png"
                 width={45.5}
@@ -53,7 +64,7 @@ const Techno = () => {
                 alt="react"
               />
             </div>
-            <div className="logo-container-one-techno logo-container-techno">
+            <div className="logo-container-o">
               <Image
                 src="/logos/strapi.png"
                 width={140}
@@ -61,7 +72,7 @@ const Techno = () => {
                 alt="strapi"
               />
             </div>
-            <div className="logo-container-one-techno logo-container-techno">
+            <div className="logo-container-o">
               <Image
                 src="/logos/shopify.png"
                 width={127}
@@ -69,7 +80,7 @@ const Techno = () => {
                 alt="shopify"
               />
             </div>
-            <div className="logo-container-one-techno logo-container-techno">
+            <div className="logo-container-o">
               <Image
                 src="/logos/wordpress.png"
                 width={45.5}
@@ -78,8 +89,8 @@ const Techno = () => {
               />
             </div>
           </div>
-          <div className="w-full flex justify-between items-center mt-7">
-            <div className="logo-container-one  logo-container-techno-Two">
+          <div className="w-full hidden md:flex justify-between items-center mt-7">
+            <div className="logo-container-one">
               <Image
                 src="/logos/tailwind.png"
                 width={117}
@@ -87,7 +98,7 @@ const Techno = () => {
                 alt="tailwind"
               />
             </div>
-            <div className="logo-container-techno-one logo-container-techno-two">
+            <div className="logo-container-techno-on">
               <Image
                 src="/logos/symfony.png"
                 width={36}
@@ -95,7 +106,7 @@ const Techno = () => {
                 alt="symfony"
               />
             </div>
-            <div className="logo-container-one-techno logo-container-techno-two">
+            <div className="logo-container-one-techn">
               <Image
                 src="/logos/firebase.png"
                 width={202.5}
@@ -103,7 +114,7 @@ const Techno = () => {
                 alt="firebase"
               />
             </div>
-            <div className="logo-container-one  logo-container-techno-two">
+            <div className="logo-container-one">
               <Image
                 src="/logos/google.png"
                 width={37}
@@ -111,6 +122,143 @@ const Techno = () => {
                 alt="google"
               />
             </div>
+          </div>
+          <div className="flex md:hidden justify-center items-center mt-7">
+            <Carousel
+              plugins={[plugin.current]}
+              className="w-full max-w-xs"
+              // onMouseEnter={plugin.current.stop}
+              // onMouseLeave={plugin.current.reset}
+            >
+              <CarouselContent>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/next.png"
+                          width={90}
+                          height={90}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/react.png"
+                          width={90}
+                          height={90}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/strapi.png"
+                          width={140}
+                          height={33}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/shopify.png"
+                          width={127}
+                          height={48}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/wordpress.png"
+                          width={90}
+                          height={90}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/tailwind.png"
+                          width={117}
+                          height={41}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/symfony.png"
+                          width={90}
+                          height={90}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/firebase.png"
+                          width={202.5}
+                          height={44.5}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <Image
+                          src="/logos/google.png"
+                          width={74}
+                          height={58}
+                          alt="next"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+            </Carousel>
           </div>
         </div>
       </div>
