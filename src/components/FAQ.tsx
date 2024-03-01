e7550a6e0b25ba59aa66f8044c07d0448d231ac1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-01 16:02:20 +0100

    Update FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -53,122 +53,124 @@ const FAQ = () => {
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Quelles technologies utilisez-vous pour le développement web ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Notre équipe maîtrise une large gamme de technologies, notamment
+                Next.js, React.js, WordPress, Shopify, et Node.js, pour répondre
+                aux besoins variés de nos clients, que ce soit pour des sites
+                vitrines, des sites e-commerce, des pages d&apos;atterrissage ou
+                des simulateurs.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-2">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Proposez-vous des solutions personnalisées pour chaque projet ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Absolument ! Nous adoptons une approche sur-mesure pour chaque
+                projet, en commençant par une analyse détaillée de vos besoins
+                pour concevoir et développer une solution qui correspond
+                parfaitement à vos attentes et objectifs.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-3">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Travaillez-vous avec des clients de tous les secteurs ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Oui, nous sommes fiers de travailler avec des clients de divers
+                secteurs. Notre expertise nous permet de comprendre et de
+                répondre efficacement aux besoins spécifiques de chaque domaine.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-4">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Quel est votre processus de développement web ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Notre processus débute par la compréhension de vos objectifs,
+                suivi par la planification, la conception, le développement, les
+                tests, et enfin, le déploiement de votre projet, tout en
+                assurant une communication transparente à chaque étape.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-5">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Comment assurez-vous la qualité et la sécurité des sites web
+                développés ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                La qualité et la sécurité sont nos priorités. Nous suivons les
+                meilleures pratiques de codage, réalisons des tests rigoureux et
+                appliquons des mesures de sécurité à jour pour assurer la
+                robustesse et la protection de votre site web.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-6">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Offrez-vous des services de maintenance et de support
+                post-lancement ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Oui, nous proposons des services de maintenance et de support
+                pour garantir le bon fonctionnement de votre site web après son
+                lancement, incluant des mises à jour régulières et des
+                vérifications de sécurité.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-7">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Comment gérez-vous les projets à distance avec les clients ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Nous utilisons des outils de communication et de gestion de
+                projet en ligne pour assurer une collaboration efficace et
+                transparente avec nos clients, peu importe leur emplacement
+                géographique.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-8">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Quels sont les délais de réalisation pour un projet ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Les délais peuvent varier en fonction de la complexité et de la
+                portée du projet. Nous travaillons étroitement avec vous pour
+                établir un calendrier réaliste et nous nous engageons à
+                respecter les délais convenus.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-9">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Comment déterminez-vous le coût d&apos;un projet ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Le coût est déterminé en fonction de la complexité, de la
+                technologie utilisée, et de la portée du projet. Nous
+                fournissons un devis détaillé après une évaluation complète de
+                vos besoins.
               </AccordionContent>
             </AccordionItem>
+
             <AccordionItem value="item-10">
               <AccordionTrigger>
-                Quels sont les technologies que vous proposez ?{" "}
+                Avez-vous des études de cas ou des références clients que je
+                peux consulter ?
               </AccordionTrigger>
               <AccordionContent>
-                Nous travaillons aussi bien sur des site vitrine, des
-                simulateur, des sites ecommerces. Soit en partant de rien (from
-                scratch) soit en utilisant votre CMS préféré, nos équipes
-                s’adapte à vos besoins afin de vous proposer un service
-                sur-mesure du design au développement.
+                Oui, nous avons un portfolio de projets réussis et des études de
+                cas que nous serions ravis de partager avec vous. Cela peut vous
+                donner une meilleure idée de notre expertise et de la façon dont
+                nous pourrions contribuer à la réussite de votre projet.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
